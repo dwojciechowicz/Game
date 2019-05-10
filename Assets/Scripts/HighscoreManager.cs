dfae2344c6c683da05b24eee2a75c3a098a23b55
dwojciechowicz <48378186+dwojciechowicz@users.noreply.github.com>
2019-05-10 11:05:30 +0200

    12. HighscoreMenu

---
+++ Assets/Scripts/HighscoreManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class HighscoreManager : MonoBehaviour
+{
+    public Text highScore;
+    // Start is called before the first frame update
+    
+    // Update is called once per frame
+    void Update()
+    {
+        highScore.text = "Highscore: " + ((int)PlayerPrefs.GetFloat("highscore")).ToString();
+    }
+    public void ToMenu()
+    {
+        SceneManager.LoadScene("Menu");
+    }
+    public void ResetHighscore()
+    {
+        PlayerPrefs.SetFloat("highscore", 0);
+    }
+}
--- Assets/Scripts/MenuManager.cs
+++ Assets/Scripts/MenuManager.cs
@@ -6,16 +6,16 @@ using UnityEngine.UI;
 
 public class MenuManager : MonoBehaviour
 {
-    public Text highScore;
+    
     // Start is called before the first frame update
-    void Start()
-    {
-        highScore.text = "Highscore: " + ((int)PlayerPrefs.GetFloat("highscore")).ToString();
-    }
-
+   
     public void ToLevel1()
     {
         SceneManager.LoadScene("Level1");
+    }
+    public void ToHighscore()
+    {
+        SceneManager.LoadScene("Highscore");
     }
     public void ExitGame()
     {
